eaa67adcbf3ccaf8163f092ee7cd3375b02fc250
Adam Lapidoth <adamlapidoth@gmail.com>
2023-05-27 08:22:40 +0300

    used black on project

--- profiles_api/migrations/0001_initial.py
+++ profiles_api/migrations/0001_initial.py
@@ -38,8 +38,8 @@ class Migration(migrations.Migration):
                     models.BooleanField(
                         default=False,
                         help_text="Designates that this user has all"
-                                  " permissions without explicitly assigning"
-                                  " them.",
+                        " permissions without explicitly assigning"
+                        " them.",
                         verbose_name="superuser status",
                     ),
                 ),
@@ -52,8 +52,8 @@ class Migration(migrations.Migration):
                     models.ManyToManyField(
                         blank=True,
                         help_text="The groups this user belongs to. A user"
-                                  " will get all permissions granted to each "
-                                  "of their groups.",
+                        " will get all permissions granted to each "
+                        "of their groups.",
                         related_name="user_set",
                         related_query_name="user",
                         to="auth.Group",
